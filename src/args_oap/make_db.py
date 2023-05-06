fd9f115b34b34c9aa4af16a93c867105a83cd713
mrning <ning@mrning.com>
2023-05-06 14:48:31 +0800

    adapt for BLAST(ignore blank space error) and bwa(ignore stderr log on Windows)

--- src/args_oap/make_db.py
+++ src/args_oap/make_db.py
@@ -20,7 +20,7 @@ def make_db(file,logger):
     
     if subp.returncode == 1: # auto choose dbtype by checking diamond's return code
         if subp.stderr == b'Error: The sequences are expected to be proteins but only contain DNA letters. Use the option --ignore-warnings to proceed.\n' or subp.stderr == b'Error: The sequences are expected to be proteins but only contain DNA letters. Use the option --ignore-warnings to proceed.\r\n':
-            subprocess.run(['bwa', 'index', file], check=True, stderr=subprocess.DEVNULL, creationflags=subprocess.CREATE_NO_WINDOW if os.name=='nt' else 0)
+            subprocess.run(['bwa', 'index', file], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, creationflags=subprocess.CREATE_NO_WINDOW if os.name=='nt' else 0)
             dbtype = 'nucl'
         else:
             logger.critical('Cannot build diamond database of <{}> ({}). Please check input file (-i/--infile).'.format(file, subp.stderr))
@@ -28,8 +28,8 @@ def make_db(file,logger):
     else:
         dbtype = 'prot'
 
-    subprocess.run(['makeblastdb', '-in', os.path.basename(file), '-dbtype', dbtype, '-out', os.path.basename(file)], cwd=os.path.dirname(file), check=True, stdout=subprocess.DEVNULL, creationflags=subprocess.CREATE_NO_WINDOW if os.name=='nt' else 0)
+    subprocess.run(['makeblastdb', '-in', os.path.basename(file), '-dbtype', dbtype, '-out', os.path.basename(file)], cwd=os.path.dirname(file), check=False, stdout=subprocess.DEVNULL, creationflags=subprocess.CREATE_NO_WINDOW if os.name=='nt' else 0)
     logger.info('Finished.')
 
 def run_make_db(options):
-    make_db(vars(options).get('infile'))
+    make_db(vars(options).get('infile'),vars(options).get('logger'))
